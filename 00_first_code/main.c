f3b0016234deb02405cf31e96bd5e5e59fd5c536
Anton Gerdelan <antonofnote@gmail.com>
2017-10-05 00:22:41 +0100

    command buffer creation

--- 00_first_code/main.c
+++ 00_first_code/main.c
@@ -20,13 +20,19 @@
 typedef struct vk_session_t {
   VkInstance instance;
 
-  VkPhysicalDevice *physical_devices; // TODO(allocate mem for this)
-  uint32_t nphysical_devices;         // gpu count
+  VkPhysicalDevice *physical_devices;
+  uint32_t nphysical_devices;
 
   VkDevice logical_device;
 
-  VkQueueFamilyProperties *queue_family_properties; // TODO(allocate mem for this)
+  VkQueueFamilyProperties *queue_family_properties;
   uint32_t nqueue_family_properties;
+
+  uint32_t queue_family_index;
+
+  VkCommandPool command_pool;
+  // Note: could be an array of buffers
+  VkCommandBuffer command_buffer;
 } vk_session_t;
 
 // returns false on error, true on warning or success
@@ -137,12 +143,8 @@ bool start_vulkan( vk_session_t *vk_session, const char *app_short_name ) {
         printf("  heap size: %lu\n", (long unsigned int)mem_properties.memoryHeaps[j].size);
       }
     }
-
+    // TODO note there is also GetPhysicalDeviceFeatures
   }
-  { // TODO(Anton) query hardware. VRAM size etc and set device preferences
-		// "You can query the GPUs names, properties, capabilities, etc. For example see vkGetPhysicalDeviceProperties and vkGetPhysicalDeviceFeatures.
-
-	}
   { // create 'logical' device NOTE: using device 0 as default here
     int device_to_use = 0;
 
@@ -177,6 +179,7 @@ bool start_vulkan( vk_session_t *vk_session, const char *app_short_name ) {
       if ( vk_session->queue_family_properties[i].queueFlags & VK_QUEUE_GRAPHICS_BIT ) {
         queue_info.queueFamilyIndex = i;
         found                       = true;
+        vk_session->queue_family_index = i; // used later in command buffer pool
         break;
       }
     }
@@ -199,6 +202,36 @@ bool start_vulkan( vk_session_t *vk_session, const char *app_short_name ) {
       return false;
     }
   }
+    { // create a command buffer. note after populating with commands must call vkQueueSubmit()
+      VkCommandPoolCreateInfo cmd_pool_info = {
+        .sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO,
+        .pNext = NULL,
+        .queueFamilyIndex = vk_session->queue_family_index,
+        .flags = 0
+      };
+      
+      VkResult res = vkCreateCommandPool(vk_session->logical_device, &cmd_pool_info, NULL, &vk_session->command_pool);
+      if ( !check_vk_errors( res, __FILE__, __LINE__ ) ) {
+        return false;
+      }
+
+      VkCommandBufferAllocateInfo cmd_info = {
+        .sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO,
+        .pNext = NULL,
+        .commandPool = vk_session->command_pool,
+        .level = VK_COMMAND_BUFFER_LEVEL_PRIMARY,
+        .commandBufferCount = 1};
+      
+      res = vkAllocateCommandBuffers(vk_session->logical_device, &cmd_info, &vk_session->command_buffer);
+      if ( !check_vk_errors( res, __FILE__, __LINE__ ) ) {
+        return false;
+      }
+    }
+    { // create swapchain (image buffers for swapping/display). platform-specific stuff is in extensions like KHR
+
+    }
+
+
   
   // "The overall picture is that your initialisation mostly looks like vkCreateInstance() → vkEnumeratePhysicalDevices() → vkCreateDevice().
   // For a quick and dirty hello world triangle program, you can do just that and pick the first physical device, then come back to it
